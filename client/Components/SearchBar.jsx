a33c7b5e86784c45cbbcfe29154339f862308882
Kevin Park-Lee <kevin38424@gmail.com>
2022-06-15 15:03:43 -0400

    updated Search Bar

--- client/Components/SearchBar.jsx
+++ client/Components/SearchBar.jsx
@@ -2,27 +2,66 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import { Select } from '@mantine/core';
 
 
-export default function SearchBar({updateMake, updateModel, updateYear, updateZip, fetching}) {
+
+export default function SearchBar({
+  updateMake,
+  make,
+  updateModel,
+  modelList,
+  updateYear,
+  updateZip,
+  fetching,
+}) {
   return (
     <Box
-    component="form"
-    sx={{
-      '& > :not(style)': { m: 1, width: '25ch' },
-      display: 'flex',
-      justifyContent: 'center',
-      flexWrap: 'wrap',
-      margin: 2
-    }}
-    noValidate
-    autoComplete="off"
-  >
-    <TextField onChange={updateMake} color="secondary" label="Make" variant="outlined" />
-    <TextField onChange={updateModel} id="outlined-basic" label="Model" variant="outlined" />
-    <TextField onChange={updateYear} id="outlined-basic" label="Minimum Year" variant="outlined" />
-    <TextField onChange={updateZip} id="outlined-basic" label="Zip" variant="outlined" />
-    <Button variant="contained" color="success" onClick={fetching}>Search</Button>
-  </Box>
-  )
+      component='form'
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        margin: 2,
+      }}
+      noValidate
+      autoComplete='off'
+    >
+      <Select
+        label='Make'
+        placeholder='Select Make'
+        value={make}
+        data={[
+          { value: 'honda', label: 'Honda' },
+          { value: 'toyota', label: 'Toyota' },
+          { value: 'tesla', label: 'Tesla' },
+          { value: 'ford', label: 'Ford' },
+          { value: 'audi', label: 'Audi' },
+        ]}
+        onChange={updateMake}
+      />
+      <Select
+        label='Model'
+        placeholder='Select Model'
+        data={modelList}
+        onChange={updateModel}
+      />
+      <TextField
+        onChange={updateYear}
+        id='outlined-basic'
+        label='Minimum Year'
+        variant='outlined'
+      />
+      <TextField
+        onChange={updateZip}
+        id='outlined-basic'
+        label='Zip'
+        variant='outlined'
+      />
+      <Button variant='contained' color='success' onClick={fetching}>
+        Search
+      </Button>
+    </Box>
+  );
 }
