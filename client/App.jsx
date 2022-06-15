a5b039a29f19bd89845fa4c4204137e04c503761
Jin Yoo <iyoojin@gmail.com>
2022-06-15 13:14:35 -0400

    dropdown_make_model_done

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import MainContainer from './containers/MainContainer'
+import React, { useState } from 'react';
+import MainContainer from './containers/MainContainer';
 import { Routes, Route } from 'react-router-dom';
 // import Trends from './containers/Trends';
 import CarsInfo from './containers/CarsInfo';
@@ -7,69 +7,103 @@ import SearchBar from './Components/SearchBar';
 import Wishlist from './containers/Wishlist';
 
 const App = () => {
-    const [carsCom, setCarsCom] = useState([]);
-    const [autoTrader, setAutoTrader] = useState([]);
-    const [trueCar, setTrueCar] = useState([]);
-    const [make, setMake] = useState('');
-    const [model, setModel] = useState('');
-    const [year, setYear] = useState(2022);
-    const [zip, setZip] = useState(10001);
-  
-    const updateMake = (e) => {
-      setMake(e.target.value)
+  const [carsCom, setCarsCom] = useState([]);
+  const [autoTrader, setAutoTrader] = useState([]);
+  const [trueCar, setTrueCar] = useState([]);
+  const [make, setMake] = useState('');
+  const [model, setModel] = useState('');
+  const [year, setYear] = useState(2022);
+  const [zip, setZip] = useState(10001);
+  const [modelList, setModelList] = useState([]);
+
+  console.log('make:', make, 'modelList:', modelList);
+
+  const updateMake = (e) => {
+    setMake(e);
+    if (e === 'honda') {
+      setModelList([
+        { value: 'accord', label: 'Accord' },
+        { value: 'civic', label: 'Civic' },
+      ]);
+      console.log('this is modellist from app.jsx: ', modelList);
     }
-    const updateYear = (e) => {
-      setYear(e.target.value)
+    if (e === 'toyota') {
+      setModelList([
+        { value: 'camry', label: 'Camry' },
+        { value: 'supra', label: 'Supra' },
+      ]);
     }
-    const updateModel = (e) => {
-      setModel(e.target.value)
+    if (e === 'tesla') {
+      setModelList([
+        { value: 'model 3', label: 'Model 3' },
+        { value: 'model y', label: 'model Y' },
+      ]);
     }
-    const updateZip = (e) => {
-      setZip(e.target.value)
+    if (e === 'ford') {
+      setModelList([
+        { value: 'focus', label: 'Focus' },
+        { value: 'bronco', label: 'Bronco' },
+      ]);
     }
-  
-    const fetching = () => {
-      fetch(`/api/scrape/${make}/${model}/${year}/${zip}`)
-        .then(response => response.json())
-        .then(data => {
-          setCarsCom(data.carsComData)
-          setAutoTrader(data.autoTraderData)
-          setTrueCar(data.trueCarData)
-        })
-        .catch(err => console.log("Error in fetch App.jsx:", err))
+    if (e === 'audi') {
+      setModelList([
+        { value: 'a4', label: 'A4' },
+        { value: 'q7', label: 'Q7' },
+      ]);
     }
-    
-    return (
+  };
+  const updateYear = (e) => {
+    setYear(e.target.value);
+  };
+  const updateModel = (e) => {
+    setModel(e);
+  };
+  const updateZip = (e) => {
+    setZip(e.target.value);
+  };
+
+  const fetching = () => {
+    fetch(`/api/scrape/${make}/${model}/${year}/${zip}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCarsCom(data.carsComData);
+        setAutoTrader(data.autoTraderData);
+        setTrueCar(data.trueCarData);
+      })
+      .catch((err) => console.log('Error in fetch App.jsx:', err));
+  };
+
+  return (
     <div id='app'>
-        <MainContainer />
-        <Routes>
-            <Route
-                exact path="/"
-                element={
-                <div>
-                    <SearchBar 
-                        updateMake={updateMake}
-                        updateYear={updateYear}
-                        updateModel={updateModel}
-                        updateZip={updateZip}
-                        fetching={fetching}
-                    />
-                    <CarsInfo
-                        carsCom={carsCom}
-                        autoTrader={autoTrader}
-                        trueCar={trueCar}
-                    />
-                </div>
-            }
-            />
-            <Route
-                exact path="/wishlist"
-                element={
-                <Wishlist />
-            }
-            />
-        </Routes>
+      <MainContainer />
+      <Routes>
+        <Route
+          exact
+          path='/'
+          element={
+            <div>
+              <SearchBar
+                updateMake={updateMake}
+                updateYear={updateYear}
+                updateModel={updateModel}
+                updateZip={updateZip}
+                fetching={fetching}
+                setModelList={setModelList}
+                modelList={modelList}
+                // updateModelList={updateModelList}
+              />
+              <CarsInfo
+                carsCom={carsCom}
+                autoTrader={autoTrader}
+                trueCar={trueCar}
+              />
+            </div>
+          }
+        />
+        <Route exact path='/wishlist' element={<Wishlist />} />
+      </Routes>
     </div>
-)};
+  );
+};
 
 export default App;
--- client/Components/SearchBar.jsx
+++ client/Components/SearchBar.jsx
@@ -1,28 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import { Select } from '@mantine/core';
+// import { Co2Sharp } from '@mui/icons-material';
 
-
-export default function SearchBar({updateMake, updateModel, updateYear, updateZip, fetching}) {
+export default function SearchBar({
+  updateMake,
+  make,
+  updateModel,
+  modelList,
+  updateYear,
+  updateZip,
+  fetching,
+}) {
   return (
     <Box
-    component="form"
-    sx={{
-      '& > :not(style)': { m: 1, width: '25ch' },
-      display: 'flex',
-      justifyContent: 'center',
-      flexWrap: 'wrap',
-      margin: 2
-    }}
-    noValidate
-    autoComplete="off"
-  >
-    <TextField onChange={updateMake} color="secondary" label="Make" variant="outlined" />
-    <TextField onChange={updateModel} id="outlined-basic" label="Model" variant="outlined" />
-    <TextField onChange={updateYear} id="outlined-basic" label="Minimum Year" variant="outlined" />
-    <TextField onChange={updateZip} id="outlined-basic" label="Zip" variant="outlined" />
-    <Button variant="contained" color="success" onClick={fetching}>Search</Button>
-  </Box>
-  )
+      component='form'
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        margin: 2,
+      }}
+      noValidate
+      autoComplete='off'
+    >
+      <Select
+        label='Make'
+        placeholder='Select Make'
+        value={make}
+        data={[
+          { value: 'honda', label: 'Honda' },
+          { value: 'toyota', label: 'Toyota' },
+          { value: 'tesla', label: 'Tesla' },
+          { value: 'ford', label: 'Ford' },
+          { value: 'audi', label: 'Audi' },
+        ]}
+        onChange={updateMake}
+      />
+      <Select
+        label='Model'
+        placeholder='Select Model'
+        data={modelList}
+        onChange={updateModel}
+      />
+      <TextField
+        onChange={updateYear}
+        id='outlined-basic'
+        label='Minimum Year'
+        variant='outlined'
+      />
+      <TextField
+        onChange={updateZip}
+        id='outlined-basic'
+        label='Zip'
+        variant='outlined'
+      />
+      <Button variant='contained' color='success' onClick={fetching}>
+        Search
+      </Button>
+    </Box>
+  );
 }
+
+// const cars = {
+//   makes: [
+//     {id: 1,
+//       name: Honda},
+//     {id: 2,
+//       name: Ford},
+//     {id: 3,
+//       name: Tesla},
+//   ],
+//   models:[
+//     {
+//       id: 1,
+//       name: Accord,
+//       makes: 1
+//     },
+//   ]
+// }
